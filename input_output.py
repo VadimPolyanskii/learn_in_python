910b6988273e80863dde624f302fe8d3c491c741
Vad Pol <work.arb19@gmail.com>
2021-10-01 17:21:21 +0300

    Input_output

---
+++ input_output.py
@@ -0,0 +1,24 @@
+# Ввод, вывод, комментарии
+
+# Однострочный комментарий
+
+'''
+
+первая строка
+вторая строка
+...
+
+'''
+
+# Вывод ----------------
+print('Hello!')
+print('Hello!', 'Student', 123)
+print('Hello!', 'Student', 123, sep = '---')
+print('Hello!', 'Student', 123, end = '!!!')
+print()
+
+# Ввод -----------------
+age = input('Input your age')
+
+print(age, type(age))
+print(int(age), type(age))
