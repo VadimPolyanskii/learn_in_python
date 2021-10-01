b30786395019cd8d1cd48cbe7ecb6966853e8a1b
Vad Pol <work.arb19@gmail.com>
2021-10-01 17:45:49 +0300

    Math operations

---
+++ math_operations.py
@@ -0,0 +1,37 @@
+# Математические операции
+
+a = input('First number')
+b = input('Second number')
+
+a = int(a)
+b = int(b)
+
+print(a+b)
+print(a-b)
+print(a*b)
+result = a / b
+print(result, type(result))
+
+print(a**2)
+
+# Логические операции
+
+a = True
+b = False
+
+# Отрицание
+print(not a)
+
+# Логическое "и"
+print(a and b)
+
+# Логическое "или"
+print(a or b)
+
+a = 10
+print(a > 100)
+print(a < 100)
+print(a <= 100)
+print(a >= 100)
+print(a == 100)
+print(a != 100)
